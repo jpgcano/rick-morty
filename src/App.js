f7aea4e600cc9c22d24ad9b5523879c5389ee2a2
jpgcano <juan-0395@hotmail.com>
2024-09-27 23:20:07 -0500

    subiendo proyecto de reat a git

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import logo from './logo.svg';
+import imagenRickMorty from "./img/rick-morty.png"
 import './App.css';
+import { useState } from "react";
+import Characters from "./componentes/characters";
+
 
 function App() {
+  const [charactes, setCharactes] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character/');
+    const characterApi = await api.json();
+
+    setCharactes(characterApi.results);
+
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className='title' >Rick & morty </h1>
+        {
+          charactes ?
+            (<Characters characters={charactes} setCharacters={setCharactes}  />)
+            : <>
+              <img className='img-home' src={imagenRickMorty} alt='Rick and morty' ></img>
+              <button onClick={reqApi} className="btn-search" >Buscar personajes </button>
+            </>
+        }
+
       </header>
     </div>
   );
